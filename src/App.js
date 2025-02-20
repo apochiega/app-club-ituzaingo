a234a4f81228dc9cef1e02ba2f7ae2a91e4b3e97
Joaco4200 <joaquinscarone1012@gmail.com>
2025-02-19 21:56:00 -0300

    Instalo react-router-dom y bootstrap. Empiezo con la  UserPage y manejo las rutas de homepage y userpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Pages/HomePage/HomePage';
+import UserPage from './Pages/UserPages/UserPage';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserPage/>}/>
+          <Route path="/HomePage" element={<HomePage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = ()=>{
 
+    const navigate= useNavigate();
+    
+    const handleClick = ()=>{
+        navigate("/HomePage");
+    }
+    
     return(
         <div className="d-flex justify-content-between align-items-center">
             <div className="">
                 <img className="w-25"  src="/padelogo.png" alt="logo"></img>
             </div>
             <div className="d-flex gap-3">
-                <button className="btn btn-success btn-lg " alt="log in">Inicio</button>
+                <button className="btn btn-success btn-lg" onClick={handleClick} alt="log in">Inicio</button>
                 <button className="btn btn-danger btn-lg ml-2" alt="log out">Logout</button>
             </div>
         </div>
