33f5d12df368676ef154520594ef972f091f0e7c
JulianaHernandesC <julianahernandez497@gmail.com>
2025-02-21 08:22:41 -0300

    package

--- src/Pages/PackagePage/packagePage.jsx
+++ src/Pages/PackagePage/packagePage.jsx
@@ -3,7 +3,7 @@ import "./packagepage.css";
 
 function PackagePage() {
     return (
-        <div>
+        <div className="container"> 
             <Header />
             <h1 className="titlePackage"> Administrar paquetes de cupones</h1>
         </div>
