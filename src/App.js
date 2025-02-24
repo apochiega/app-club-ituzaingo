bc1f85a98bb45b3356ada7ef7bd47ea9ebdd292a
JulianaHernandesC <julianahernandez497@gmail.com>
2025-02-24 10:08:25 -0300

    tabla usuarios y pagina usuarios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Pages/HomePage/HomePage';
-import UserPage from './Pages/UserPages/UserPage';
+import UserPage from './Pages/UserPage/UserPage';
 import PackagePage from './Pages/PackagePage/packagePage';
+import StickyHeadTable from './Components/Tables/userTable';
 
 function App() {
   return (
--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import { useState } from "react";
+import { useEffect } from "react";
+import StickyHeadTable from "../../Components/Tables/userTable";
+import "./UserPage.css";
 
 const UserPage = ()=>{
 
@@ -12,12 +15,12 @@ const UserPage = ()=>{
     };
     
     return(
-        <div className="container">
+        <div className="containerUser">
             <Header/>
             <div className="fs-2 fw-bold">
                 Usuarios
             </div>
-            <div className="d-flex gap-3 mt-5 w-50">
+            <div className="d-flex gap-3 mt-4 w-50">
                 <input
                 type="text"
                 className="form-control w-50"
@@ -25,14 +28,10 @@ const UserPage = ()=>{
                 onChange={(e) => setUserNumber(e.target.value)}
                 placeholder="Ingrese el numero de socio"
                 />
-                <input
-                type="text"
-                className="form-control w-50"
-                value={cedula}
-                onChange={(e) => setCedula(e.target.value)}
-                placeholder="Ingrese la cedula"
-                />
                 <button className="btn btn-success" onClick={handleSearchUser}> Buscar </button>
+            </div>
+            <div className="mt-5 ">
+                <StickyHeadTable/>
             </div>
         </div>
     )
