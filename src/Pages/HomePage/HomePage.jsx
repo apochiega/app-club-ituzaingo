2be9a640c65c99520cc959f973a490aedf9f0da2
apochiega <julianpijuan15@gmail.com>
2025-02-25 09:35:10 -0300

    error fixed

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomePage.css"
-import ImageButton from "../../Components/imageButton/ImageButton";
+import ImageButton from "../../Components/ImageButton/ImageButton";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
