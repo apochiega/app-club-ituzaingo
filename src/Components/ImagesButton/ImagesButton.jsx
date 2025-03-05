0102c1ea0b22a1a30a948cd5367161162264b61d
apochiega <julianpijuan15@gmail.com>
2025-03-05 20:08:14 -0300

    Last details

--- src/Components/ImagesButton/ImagesButton.jsx
+++ src/Components/ImagesButton/ImagesButton.jsx
@@ -1,16 +1,18 @@
-import "./ImagesButton.css"
+import "./ImagesButton.css";
 
 function ImagesButton({ text, image, onClick }) {
   return (
     <button className="container-image-text" onClick={onClick}>
-      <div className="image-wrapper">
-        <img className="imagen-cancha" src={image || "/placeholder.svg"} alt="cancha padel" />
-        <div className="overlay"></div>
+      <div className="image-container">
+        <img
+          className="imagen-cancha"
+          src={image || "/placeholder.svg"}
+          alt="icono"
+        />
       </div>
       <p className="texto-paquete">{text}</p>
     </button>
-  )
+  );
 }
 
-export default ImagesButton
-
+export default ImagesButton;
